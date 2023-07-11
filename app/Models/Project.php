0135574b4538043b272b408cea8e35a897b4ff7c
StefanoGalandrini <stefe68@gmail.com>
2023-07-11 15:22:09 +0200

    Add Models functions for Project and Technology

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Technology;
 use App\Models\Type;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -21,5 +22,10 @@ class Project extends Model
     public function type()
     {
         return $this->belongsTo(Type::class);
+    }
+
+    public function technologies()
+    {
+        return $this->belongsToMany(Technology::class);
     }
 }
--- app/Models/Technology.php
+++ app/Models/Technology.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Project;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,4 +11,9 @@ class Technology extends Model
     use HasFactory;
 
     public $timestamps = false;
+
+    public function projects()
+    {
+        return $this->belongsToMany(Project::class);
+    }
 }
