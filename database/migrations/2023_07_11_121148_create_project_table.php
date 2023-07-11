1c1df4030c14b730909ff0627f6affe72414eae2
StefanoGalandrini <stefe68@gmail.com>
2023-07-11 14:21:07 +0200

    Add new database and new migration for projects

---
+++ database/migrations/2023_07_11_121148_create_project_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+
+            // Add Foreign Key column
+            $table->unsignedBigInteger('type_id');
+
+            $table->string('title', 100);
+            $table->string('url_image', 200);
+            $table->text('description');
+            $table->date('creation_date');
+            $table->string('url_repo', 200);
+
+            // define column as Foreign Key
+            $table->foreign('type_id')->references('id')->on('types');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+};
